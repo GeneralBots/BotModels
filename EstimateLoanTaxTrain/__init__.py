217345a4fba8436d0423e9434028ceeb350d5404
Rodrigo Rodriguez <me@rodrigorodriguez.com>
2021-05-06 19:14:04 -0300

    new(all): Trainning project added.

--- EstimateLoanTaxTrain/__init__.py
+++ EstimateLoanTaxTrain/__init__.py
@@ -1,22 +1,15 @@
 import logging
 
 import azure.functions as func
-#set STORAGE_BLOB_URL='https://pythonazurestorage12345.blob.core.windows.net'
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # https://docs.microsoft.com/pt-br/azure/developer/python/azure-sdk-example-storage-use?tabs=cmd
     logging.info('Python HTTP trigger function processed a request.')
 
-    age = req.params.get('age')
-    income = req.params.get('income')
-    employeeTime = req.params.get('employeeTime')
+    name = req.params.get('name')
     
-    # Code here.
-
-    returnedTax = 1;
-
     if name:
-        return func.HttpResponse(returnedTax)
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
